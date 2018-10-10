ff9aea93e4d76ca0cc72464c4671c8e82fbacda7
LalitaShrivasta <lalita17@navgurukul.org>
2018-10-11 00:55:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import emogis from "./components/emogis.json";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputfield: ""
+    };
+  }
+
+  onsearchinput = e => {
+    this.setState({ inputfield: e.target.value });
+    console.log(e.target.value);
+  };
+
   render() {
+    const { inputfield } = this.state;
+    const filtermethod = emogis.filter(data => {
+      if (
+        data.title.toLowerCase().indexOf(inputfield.toLocaleLowerCase()) !== -1
+      )
+        return true;
+      if (data.keywords.toLowerCase().indexOf(inputfield.toLowerCase()) !== -1)
+        return true;
+      return false;
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input vlaue={this.state.search} onChange={this.onsearchinput} />
+        <ul>
+          {filtermethod.slice(0, 20).map(function(data, index) {
+            return (
+              <div key={index}>
+                <span>{data.title}</span>
+                <span>{data.symbol}</span>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     );
   }
